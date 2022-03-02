525e8d971d33f3ea28cb17cf42d73e85e3fe9ae9
philipgarcia7 <98358696+philipgarcia7@users.noreply.github.com>
2022-03-02 16:23:06 -0600

    Update CourseProgram.py

--- CourseProgram.py
+++ CourseProgram.py
@@ -8,6 +8,7 @@ You have been given partial code. The objective is to reproduce the output as sh
    and the output should reflect that as shown in the picture.
 """
 
+import re
 import CourseClass as cc
 
 
@@ -18,18 +19,20 @@ def main():
     status = "open"
     students = ["John", "James", "Jill", "Jack", "Joanne"]
     c1 = cc.Course(name, crn, seats, status)
-
     for s in students:
+        r1 = cc.Register(s,crn)
+    for s in students:
+        r1 = cc.Register(s,crn)
         if c1.get_status() == "open":
             c1.update_seat_count()
-            print("Student Name:", s)
+            print("Student Name:", r1.get_name())
             print("Course Name:", c1.get_name())
             print("CRN:", c1.get_crn())
             print("Seat Count:", c1.get_seats())
             print()
 
         else:
-            print("Sorry ", s, ", registration is closed for ", c1.get_name(), sep="")
+            print("Sorry ", r1.get_name(), ", registration is closed for ", c1.get_name(), sep="")
 
 
 main()
