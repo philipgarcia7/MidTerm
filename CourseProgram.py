4213b35912feaa444b703a6bf4fcf2c459722a7c
Philip Garcia <philip_garcia1@baylor.edu>
2022-03-02 15:50:37 -0600

    Mid Term

--- CourseProgram.py
+++ CourseProgram.py
@@ -1,4 +1,4 @@
-'''
+"""
 You have been given partial code. The objective is to reproduce the output as shown in the file - Output.png
 1) Create an instance of the Course object
 2) Create an instance of the Register object for EACH student in the students list using a For loop.
@@ -6,23 +6,30 @@ You have been given partial code. The objective is to reproduce the output as sh
    ONLY get methods.
 4) Take note that student 'Joanne' cannot register since there are only 4 seats in the course and
    and the output should reflect that as shown in the picture.
-'''
+"""
 
+import CourseClass as cc
 
 
 def main():
-
-    name = 'MIS 4322 - Advanced Python'
-    crn = '250309'
+    name = "MIS 4322 - Advanced Python"
+    crn = "250309"
     seats = 4
-    status = 'open'
-    students = ['John','James','Jill','Jack','Joanne']
+    status = "open"
+    students = ["John", "James", "Jill", "Jack", "Joanne"]
+    c1 = cc.Course(name, crn, seats, status)
 
-    
+    for s in students:
+        if c1.get_status() == "open":
+            c1.update_seat_count()
+            print("Student Name:", s)
+            print("Course Name:", c1.get_name())
+            print("CRN:", c1.get_crn())
+            print("Seat Count:", c1.get_seats())
+            print()
+
+        else:
+            print("Sorry", s, ", registration is closed for", c1.get_name())
+
+
 main()
-
-
-
-        
-    
-    
